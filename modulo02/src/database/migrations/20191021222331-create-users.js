e991063c438e692ff5cb9863b61985414aef30d6
paulobernardo <paulo.ianes.bernardo@gmail.com>
2019-10-21 20:56:42 -0300

    Added ORM with sequelize

---
+++ modulo02/src/database/migrations/20191021222331-create-users.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      provider: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        field: 'created_at',
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        field: 'updated_at',
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('users');
+  },
+};
