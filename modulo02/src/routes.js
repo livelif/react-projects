00eef82080514b9dfb7f132547acf00cfa004087
paulobernardo <paulo.ianes.bernardo@gmail.com>
2019-10-22 19:26:22 -0300

    added subscribe to user

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,15 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
-
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Diego Fernandes',
-    email: 'paulo@rocketseat.com.br',
-    password_hash: '12345',
-  });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
