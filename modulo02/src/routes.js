a17aab404c4f0aa45d1a24d3c85d4f11c987eec9
livelif <paulo.ianes.bernardo@gmail.com>
2019-11-04 14:54:49 -0200

    added uploud files

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,16 +1,25 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 import authMiddlewares from './app/middlewares/auth';
 
 const routes = new Router();
+const uploud = multer(multerConfig);
+
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddlewares);
 routes.put('/users', UserController.update);
 
+routes.get('/providers', ProviderController.index);
+
+routes.post('/files', uploud.single('file'), FileController.store);
 
 export default routes;
